1aa0c7b15bec3a8f055422dfdafba8f586e276e1
val500 <varun.valada@gmail.com>
2018-10-20 04:18:41 -0400

    Added danger functionality to dijkstra

--- dijkstra.js
+++ dijkstra.js
@@ -5,34 +5,76 @@ const testTree = { //some tree, sample here for test
     'A': {
         exit : false,
         danger : false,
-        connections : { C:1 }
+        connections : { B:1, C:5 }
     },
     'B': {
-        exit : true,
-        danger : false,
-        connections : { C:3 }
+        exit : false,
+        danger : true,
+        connections : { A:1, C:1 }
     },
     'C': {
-        exit : false,
+        exit : true,
         danger : false,
-        connections : { B:3, A:1 }
+        connections : { B:1, A:5 }
     }
 }
+/*
+function removeDanger(tree) {
+    var danger;
+    for (var node in tree) {
+        if (tree[node].danger) {
+            danger = {node : { connections:tree[node].connections } };
+            delete tree[node];
+        }
+    }
+    for (var node in tree) {
+        for (var d in danger) {
+            for (var c in danger[d]) {
+                if (String(danger[d][connections].c) == String(node)) {
+                    delete tree[node][connections][d];
+                }
+            }
+        }
+    }
+    return tree;
+}
+*/
+function findDanger(tree) {
+    var danger = [];
+    for (var node in tree) {
+        if (tree[node].danger) {
+            danger.push(String(node));
+        }
+    }
+    return danger;
+}
+/*
+
+*/
+
 
 
 function dijkstra(startNode, tree) {
+    const danger = findDanger(tree);
     const route = new Graph();
 
     var endNode;
 
     for (var node in tree) {
-        if (tree[node].exit) {
-            endNode = String(node);
+        if (!(node in danger)) {
+            for (var c in tree[node].connections) {
+                if (danger.includes(c)) {
+                    delete tree[node].connections[c];
+                }
+            }
+            if (tree[node].exit) {
+                endNode = String(node);
+            }
+                route.addNode(String(node), tree[node].connections);
         }
-        route.addNode(String(node), tree[node].connections);
+
     }
 
     return route.path(startNode, endNode);
 }
-
 console.log(dijkstra('A', testTree))
