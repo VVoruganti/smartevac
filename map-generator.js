29194592dd7a4c467926374388170ab98c82704b
Vineeth <13438633+VVoruganti@users.noreply.github.com>
2018-10-20 03:41:43 -0400

    Ability to add nodes on click

--- map-generator.js
+++ map-generator.js
@@ -1,16 +1,51 @@
 /*The purpose of this javascript file is to create an environment that administrators can use to generate a map for their specific building */
 //var Two = require('two.js')
-document.addEventListener("DOMContentLoaded", function() {
 
+var mousePos;
+var two;
+function addNode(mouse) {
+    two.makeCircle(mouse.x,mouse.y,20);
+    two.update();
+}
+
+function getMousePos(canvas,evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
     var elem = document.getElementById('drawing-tool');
-    var two = new Two({ fullscreen: true }).appendTo(elem);
+    two = new Two({ 
+        type:Two.Types.canvas,
+        width: 500,
+        height: 480
+        
+    
+    }).appendTo(elem)
+    
+    var myCanvas = document.getElementsByTagName("canvas")[0]
+    var context = myCanvas.getContext('2d');
+
+    myCanvas.addEventListener('mousemove', function(evt) {
+     mousePos = getMousePos(myCanvas, evt);
+    }, false);
 
     var styles = {
           size: 18,
           family: 'Lato'
     } 
 
-    var rectA = two.makeRectangle(150, 100, 200, 100);
+    myCanvas.addEventListener('click', function(evt) {
+        mousePos = getMousePos(myCanvas,evt);
+        addNode(mousePos)
+    },false);
+
+        /*  var rectA = two.makeRectangle(150, 100, 200, 100);
     rectA.stroke = 'orange';
     two.makeText('Rectangle', 150, 100, styles);
 
@@ -24,10 +59,10 @@ document.addEventListener("DOMContentLoaded", function() {
     rectC.linewidth = 2;
     two.makeText('Rounded Rectangle (II)', 650, 100, styles);
 
-    var circA = two.makeCircle(150, 250, 75);
-    circA.fill = 'yellow';
-    circA.noStroke();
-    two.makeText('Circle', 150, 250, styles);
+    // var circA = two.makeCircle(150, 250, 75);
+    //circA.fill = 'yellow';
+    //circA.noStroke();
+    //two.makeText('Circle', 150, 250, styles);
 
     var ellipseA = two.makeEllipse(400, 250, 100, 75);
     ellipseA.fill = 'pink';
@@ -40,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
     ellipseB.stroke = 'black';
     ellipseB.linewidth = 20;
     two.makeText('Ellipse (II)', 650, 300, styles);
-    two.update();
+    two.update(); */
 });
 
 
