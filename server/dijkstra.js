1875f7b684a55df09d38dfb8ce93dbc3f3504b7f
Vineeth <13438633+VVoruganti@users.noreply.github.com>
2019-06-07 18:23:50 -0400

    push

---
+++ server/dijkstra.js
@@ -0,0 +1,9 @@
+/*This is the file that will be responsible for the calculations of the fastest path */
+
+/**
+ * @param {*} currentLocation - A pointer to the node that the person is currently in. 
+ * @param {*} destination - the node that needs to be reached
+ */
+function calculateDistance(currentLocation, destination) {
+
+}
